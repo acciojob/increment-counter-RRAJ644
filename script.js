fc0d82ec59b8d257a415498b75cfddcd89a16b7f
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-04 21:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,24 +2,16 @@ document.addEventListener('DOMContentLoaded', (e) => {
   const createCounterElement = () => {
     let count = 0
 
-    const counterDiv = document.createElement('div')
-
-    const counterParagraph = document.createElement('p')
-    counterParagraph.innerText = count
-
-    const incrementButton = document.createElement('button')
-    incrementButton.innerText = 'Increment'
-    incrementButton.classList.add('increment-button')
-
-    counterDiv.appendChild(counterParagraph)
-    counterDiv.appendChild(incrementButton)
+    const counterDiv = document.querySelector('#increment-counter')
+    const counterParagraph = document.getElementById('counter')
 
     counterDiv.addEventListener('click', (e) => {
       e.preventDefault()
       alert(count)
       const target = e.target.classList
 
-      if (target.contains('increment-button')) {
+      if (target.tagName === 'BUTTON' && target.innerText === 'Increment') {
+        alert(count)
         count += 1
         counterParagraph.textContent = count
       }
