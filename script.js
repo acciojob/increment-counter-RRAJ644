72ddccc115ec8d6b4c0240ff8ad30b9fdaefa803
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-04 21:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (e) => {
+  const createCounterElement = () => {
+    let count = 0
+
+    const counterDiv = document.createElement('div')
+
+    const counterParagraph = document.createElement('p')
+    counterParagraph.innerText = count
+
+    const incrementButton = document.createElement('button')
+    incrementButton.innerText = 'Increment'
+    incrementButton.classList.add('increment-button')
+
+    counterDiv.appendChild(counterParagraph)
+    counterDiv.appendChild(incrementButton)
+
+    counterDiv.addEventListener('click', (e) => {
+      e.preventDefault()
+      alert(count)
+      const target = e.target.classList
+
+      if (target.contains('increment-button')) {
+        count += 1
+        counterParagraph.textContent = count
+      }
+    })
+
+    document.body.appendChild(counterDiv)
+  }
+
+  createCounterElement()
+})
